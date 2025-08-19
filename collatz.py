e057fb9a9bb55d8c8dee80910b00cc4c7e49ed88
OrangeMan432 <167663529+OrangeMan432@users.noreply.github.com>
2025-08-18 21:14:16 -0400

    Create collatz.py (#1)

---
+++ collatz.py
@@ -0,0 +1,21 @@
+from time import perf_counter
+
+num = int(input("Input a starting number: "))
+
+count = 0
+peak_num = 0
+
+start = perf_counter()
+
+while num != 1:
+    if num % 2 == 0:
+        num = num // 2
+    else:
+        num = num * 3 + 1
+    if num >= peak_num:
+        peak_num = num
+    count += 1
+
+print(f"Completed in {perf_counter() - start} seconds.")
+print(f"Completed after {count} steps.")
+print(f"Peak number was {peak_num}.")
